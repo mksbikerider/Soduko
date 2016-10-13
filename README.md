351fb4f88ec3ac6d8219a8937b70f9f70fbff462
msutherland <mike@msutherland.name>
2016-10-13 12:57:19 +0000

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Soduko
-Soduko Solver, to allow me to try different coding practices.
+
+Sudoku Solver, to allow me to try different coding practices.
+
+This is more my scratch pad for trying new coding practices etc 
+rather than useful code. 
--- src/main/java/name/msutherland/StreamSolve.java
+++ src/main/java/name/msutherland/StreamSolve.java
@@ -61,23 +61,4 @@ public class StreamSolve implements Solve, Function<Integer[][], List<Integer[][
         return solve(cells);
     }
 
-    private class CreateMatrix implements Function<Integer, Integer[][]>{
-        private final int i;
-        private final int j;
-        private final Integer[][] cells;
-
-        private CreateMatrix(int i, int j, Integer[][] cells) {
-            this.i = i;
-            this.j = j;
-            this.cells = cells;
-        }
-
-        @Override
-        public Integer[][] apply(Integer val) {
-            Integer[][] cellsCopy = Sudoku.deepCopy(cells);
-            cellsCopy[i][j] = val;
-            return cellsCopy;
-        }
-    }
-
 }
