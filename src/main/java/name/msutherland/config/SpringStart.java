70a290083185668216ccb5a26d949dbb8fa5227f
suthermi <mike@msutherland.name>
2016-10-14 16:12:57 +0100

    Removed unused code

--- src/main/java/name/msutherland/config/SpringStart.java
+++ src/main/java/name/msutherland/config/SpringStart.java
@@ -1,22 +1,13 @@
 package name.msutherland.config;
 
-
-import name.msutherland.mvc.SudokuWebFormValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 
 @SpringBootApplication
 @ComponentScan("name.msutherland")
 public class SpringStart extends SpringBootServletInitializer {
-
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) {
-        binder.addValidators(new SudokuWebFormValidator());
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringStart.class, args);
